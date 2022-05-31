20d37a50dc8b629c824c43fb12dc25a0f5a99c5e
Aymen Akremi <aymen.akremi@gmail.com>
2022-05-31 10:15:57 +0300

    Add files via upload

---
+++ Reset.sh
@@ -0,0 +1,31 @@
+sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq)
+sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq)
+
+sleep 5
+sudo docker volume prune 
+
+sleep 5
+sudo docker volume prune
+
+# sleep 5
+# sudo docker-compose -f ./artifacts/docker-compose.yaml up -d
+sleep 5
+cd ./artifacts
+sudo docker-compose up -d
+cd ..
+
+sleep 5
+./createChannel.sh
+
+sleep 2
+
+./deployLandSACC.sh
+
+sleep 5
+sudo docker-compose -f ./artifacts/channel/create-certificate-with-ca/docker-compose.yaml up -d
+
+
+# sleep 5
+# cd ./Explorer
+# sudo docker-compose up -d
+# cd ..
